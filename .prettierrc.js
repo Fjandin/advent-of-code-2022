50da4f39d18f19b69ba745d9714ba3c43ae4c58a
René Bischoff <rene@pleo.io>
2022-12-01 17:59:21 +0100

    Batman - this commit has no parent

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+// .prettierrc.js or ..prettierrc.js
+module.exports = {
+    printWidth: 120,
+    tabWidth: 4,
+    useTabs: false,
+    semi: false,
+    singleQuote: false,
+    quoteProps: "as-needed",
+    trailingComma: "all",
+    bracketSpacing: false,
+    arrowParens: "always",
+}
---
+++ src/advent-of-code/2022/01/index.ts
@@ -0,0 +1,21 @@
+import * as fs from "fs"
+
+const calories = fs
+    .readFileSync("./input.txt")
+    .toString("utf8")
+    .split("\n\n")
+    .map((it) =>
+        it
+            .split("\n")
+            .map(Number.parseFloat)
+            .filter((it) => !isNaN(it))
+            .reduce((a, b) => a + b),
+    )
+    .sort((a, b) => a - b)
+
+console.log("top elf calories sum:", calories.slice(-1)[0])
+
+console.log(
+    "top 3 elf calories sum:",
+    calories.slice(-3).reduce((a, b) => a + b),
+)
